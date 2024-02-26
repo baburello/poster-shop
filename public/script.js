8981cd5e29e8812f58be23f2e677fb8c957bac1f
baburello <babur565@gmail.com>
2024-02-26 12:55:09 +0500

    feat: final

--- public/script.js
+++ public/script.js
@@ -1,25 +1,16 @@
+var LOAD_NUM = 4;
+var watcher;
+
 new Vue({
 	el: "#app",
 	data: {
 		total: 0,
-		products: [
-			{
-				title: "Product 1",
-				id: 1,
-				price: 9.99,
-			},
-			{
-				title: "Product 2",
-				id: 2,
-				price: 9.99,
-			},
-			{
-				title: "Product 3",
-				id: 3,
-				price: 9.99,
-			},
-		],
+		products: [],
 		cart: [],
+		search: "cat",
+		lastSearch: "",
+		loading: false,
+		results: [],
 	},
 	methods: {
 		addToCart: function (product) {
@@ -40,10 +31,58 @@ new Vue({
 				});
 			}
 		},
+		inc: function (item) {
+			item.qty++;
+			this.total += item.price;
+		},
+		dec: function (item) {
+			item.qty--;
+			this.total -= item.price;
+			if (item.qty <= 0) {
+				var i = this.cart.indexOf(item);
+				this.cart.splice(i, 1);
+			}
+		},
+
+		onSubmit: function () {
+			this.products = [];
+			this.results = [];
+			this.loading = true;
+			var path = "/search?q=".concat(this.search);
+			this.$http.get(path).then(function (response) {
+				this.results = response.body;
+				this.lastSearch = this.search;
+				this.appendResults();
+				this.loading = false;
+			});
+		},
+		appendResults: function () {
+			if (this.products.length < this.results.length) {
+				var toAppend = this.results.slice(
+					this.products.length,
+					LOAD_NUM + this.products.length
+				);
+				this.products = this.products.concat(toAppend);
+			}
+		},
 	},
 	filters: {
 		currency: function (price) {
-			return `$${price.toFixed(2)}`
+			return `$${price.toFixed(2)}`;
 		},
+	},
+	created: function () {
+		this.onSubmit();
+	},
+	updated: function () {
+		var sensor = document.querySelector("#product-list-bottom");
+		watcher = scrollMonitor.create(sensor);
+		watcher.enterViewport(this.appendResults);
+	},
+	beforeUpdate: function () {
+		if (watcher) {
+			watcher.destroy();
+			watcher = null;
+		}
 	},
 });
